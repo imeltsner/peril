7b068a51e67ce096b72a41a8e6a9bac10bbf76b8
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-11-19 15:51:16 -0800

    added ampq connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer conn.Close()
+	log.Println("Connection successful!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	log.Println("Connection closed")
 }
