b77861811c87c08f3d0f02afb047b65d4d41fda7
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-11-27 20:48:38 -0500

    added client logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer conn.Close()
+	log.Println("Connection successful!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("error getting username")
+	}
+	queueName := routing.PauseKey + "." + username
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, false)
+	if err != nil {
+		log.Fatalf("error binding: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	log.Println("Connection closed")
 }
