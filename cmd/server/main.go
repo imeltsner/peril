fcc5f17497c59bb0d45f1428df2bf554d79693c1
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-11-27 20:00:06 -0500

    added test message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,18 @@ func main() {
 	}
 	defer conn.Close()
 	log.Println("Connection successful!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("unable to create ampq channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON[routing.PlayingState](ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("unable to publish JSON %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
