83ad0b8ece31157540b48cbf13f2cd3704b3542b
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-11-27 20:49:05 -0500

    bind queue logic

---
+++ internal/pubsub/bind.go
@@ -0,0 +1,30 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType bool, // true is "durable" false is "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	q, err := ch.QueueDeclare(queueName, simpleQueueType, !simpleQueueType, !simpleQueueType, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+}
